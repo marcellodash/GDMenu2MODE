df5763186a672f80af93e6096a9c47300cce7e7c
MobiusStripTech <49572712+mobiusstriptech@users.noreply.github.com>
2020-06-23 13:08:01 -0400

    Add files via upload

---
+++ GDMenu2MODE/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GDMenu2MODE
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter the path to the game folders:");
+			Console.WriteLine(@"(Example: c:\users\gamer\gdemu");
+			string filePath = Console.ReadLine();
+			GetTheFiles(filePath);
+			Console.WriteLine("Job's Finished. Hit enter to exit.");
+			Console.ReadLine();
+		}
+
+		private static void GetTheFiles(string path)
+		{
+			DirectoryInfo di = new DirectoryInfo(path);
+			List<DirectoryInfo> directories = di.EnumerateDirectories().ToList();
+			if (directories.Count >= 1)
+			{
+				WalkDirectories(directories);
+			}
+			else
+			{
+				IEnumerable<FileInfo> files = di.EnumerateFiles();
+				string newFolderName = WalkFiles(files);
+				if (!string.IsNullOrEmpty(newFolderName))
+				{
+					UpdateDirectory(di, newFolderName);
+				}
+			}
+		}
+
+		private static void WalkDirectories(IEnumerable<DirectoryInfo> directories)
+		{
+			foreach (DirectoryInfo directory in directories)
+			{
+				IEnumerable<FileInfo> files = directory.EnumerateFiles();
+				string newFolderName = WalkFiles(files);
+				if (!string.IsNullOrEmpty(newFolderName))
+				{
+					UpdateDirectory(directory, newFolderName);
+				}
+			}
+		}
+
+		private static string WalkFiles(IEnumerable<FileInfo> files)
+		{
+			foreach (FileInfo file in files)
+			{
+				if (file.Extension.ToUpper().Equals(".TXT"))
+				{
+					string[] data = File.ReadAllLines(file.FullName);
+					return data[0];
+				}
+			}
+
+			return string.Empty;
+		}
+
+		private static void UpdateDirectory(DirectoryInfo directory, string newFolderName)
+		{
+			string currentFolderName = directory.Name;
+			char[] invalidFile = Path.GetInvalidFileNameChars();
+
+			foreach (char c in invalidFile)
+			{
+				newFolderName = newFolderName.Replace(c.ToString(), "");
+			}
+
+			string originalCompleteName = directory.FullName;
+			string newCompleteName = directory.FullName.Replace(currentFolderName, newFolderName);
+			directory.MoveTo(newCompleteName);
+			if (Directory.Exists(originalCompleteName))
+			{
+				Directory.Delete(originalCompleteName);
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # GDMenu2MODE
-Updates specified folders from the GDEMU 01, 02, etc to the Game Name.
+USE AT YOUR OWN RISK!
+It is recommended that you test on a small subset of your folders to ensure you are getting the expected results.
+
+This application updates the specified folders from the GDEMU format of 01, 02, etc to the Game Name.
+The application is built against .NET Core 3.1 and uses a simple command line interface.
+
+To use the application.
+Open the EXE.
+Enter the directory path where your images/folders are. Example: "c:\gdemu"
+Press enter and let the application update the folder names for you.
